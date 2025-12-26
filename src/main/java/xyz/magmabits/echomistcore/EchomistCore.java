7c90845245d02c63cdff94fa712832fd98873cce
Magma <sacred.lunar.dragon@gmail.com>
2025-12-27 00:11:11 +0100

    Initial commit

---
+++ src/main/java/xyz/magmabits/echomistcore/EchomistCore.java
@@ -0,0 +1,15 @@
+package xyz.magmabits.echomistcore;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EchomistCore implements ModInitializer {
+	public static final String MOD_ID = "echomist-core";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/xyz/magmabits/echomistcore/EchomistCoreClient.java
@@ -0,0 +1,10 @@
+package xyz.magmabits.echomistcore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EchomistCoreClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
